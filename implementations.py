6be31c7e64208077113ec6d40b54701c54431b8b
Florian Grötschla <florian.groetschla@student.kit.edu>
2020-10-23 10:05:06 +0200

    Changing all data (also the one used for testing) when standardizing

--- implementations.py
+++ implementations.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-def standardize(tx):
+def standardize(tx, tX_test):
 
     threshold = 0.1
     value_to_remove = -999
@@ -9,23 +9,27 @@ def standardize(tx):
     for i in range(tx.shape[1]):
 
         col = tx[:,i]
+        col_te = tX_test[:,i]
 
         clean_col = col[col != value_to_remove]
 
         mean = np.mean(clean_col, axis=0)
 
         col[col == value_to_remove] = mean
+        col_te[col_te == value_to_remove] = mean
 
         derivation = np.std(clean_col, axis=0)
 
         tx[:,i] = (col - mean)
+        tX_test[:,i] = (col_te - mean)
 
         if derivation < threshold :
             print("Warning, derivation is too small, we don't normalize by derivation the column ", i)
         else:
             tx[:,i] = tx[:,i] / derivation
+            tX_test[:,i] = tX_test[:,i] / derivation
 
-    return tx
+    return tx, tX_test
 
 def batch_iter(y, tx, batch_size, num_batches=1, shuffle=True):
     """
--- initialize.py
+++ initialize.py
@@ -3,10 +3,15 @@ from split_data import split_data
 from proj1_helpers import load_csv_data
 from implementations import standardize
 
-def initialize(data_train_path='data/train.csv', split_ratio = 0.8):
+def initialize(data_train_path='data/train.csv', split_ratio = 0.8, data_test_path='data/test.csv'):
 
     y, tX, ids = load_csv_data(data_train_path)
     y = np.expand_dims(y, axis=1)
+    
+    _, tX_test, ids_test = load_csv_data(data_test_path)
+    
+    # Standardize data
+    x_tr, tX_test = standardize(tX, tX_test)
 
     x_tr, x_te, y_tr, y_te = split_data(tX, y, split_ratio)
 
@@ -16,7 +21,4 @@ def initialize(data_train_path='data/train.csv', split_ratio = 0.8):
     w_initial = np.expand_dims(w_initial, axis=1)
     print("w = ", w_initial.shape)
 
-    # Standardize data
-    x_tr = standardize(x_tr)
-
-    return x_tr, x_te, y_tr, y_te, w_initial
+    return x_tr, x_te, y_tr, y_te, tX_test, ids_test, w_initial
