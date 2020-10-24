005db04cccf08819b43091c8f252a93358bcab40
AdrienBertaud <bertaud.adrien@gmail.com>
2020-10-24 16:31:41 +0200

    Update standardize_unit_test.py

--- unit_tests/standardize_unit_test.py
+++ unit_tests/standardize_unit_test.py
@@ -4,30 +4,58 @@ import sys
 
 sys.path.append("..") # Adds higher directory to python modules path.
 
-from implementations import get_standardization_values, apply_standardization
+from implementations import standardize
 
-def standardize_unit_test():
+x = np.array([[0, 3, 10],
+            [2, 9, 10],
+            [0, 3, 10],
+            [-999, -999, -999],
+            [2, 9, 10]])
 
-    x = np.array([[0, 3, 10],
-                    [2, 9, 10],
-                    [0, 3, 10],
-                    [-999, -999, -999],
-                    [2, 9, 10]])
+x_2 = np.array([[2, 9, -999],
+                [2, 9, 10],
+                [0, 3, 10],
+                [-999, -999, -999],
+                [2, 9, 10]])
 
-    means, derivations = get_standardization_values(x)
-    x = apply_standardization(x, means, derivations)
+x_expected = np.array([[-1, -1,  0],
+                   [ 1,  1,  0],
+                   [-1, -1,  0],
+                   [ 0,  0,  0],
+                   [ 1,  1,  0]])
 
-    print(x)
+x_expected_2 = np.array([[1, 1,  0],
+                       [ 1,  1,  0],
+                       [-1, -1,  0],
+                       [ 0,  0,  0],
+                       [ 1,  1,  0]])
 
-    x_expected = np.array([[-1, -1,  0],
-                           [ 1,  1,  0],
-                           [-1, -1,  0],
-                           [ 0,  0,  0],
-                           [ 1,  1,  0]])
-
-    if (x == x_expected).all():
+def result(value):
+    if value == True:
         print("TEST OK :)")
     else:
         print("TEST KO :(")
 
-standardize_unit_test()
+
+def standardize_unit_test_method_1(x, x_expected):
+
+    print("standardize_unit_test_method_1")
+    means, derivations = get_standardization_values(x)
+    x_std = apply_standardization(x, means, derivations)
+    print("x_std = ", x_std)
+
+    result((x_std == x_expected).all())
+
+
+def standardize_unit_test_method_2(x, x_2, x_expected, x_expected_2):
+
+    print("standardize_unit_test_method_2")
+    x_std_1, x_std_2 = standardize(x,x_2)
+
+    print("x_std_1 = ", x_std_1)
+    print("x_std_2 = ", x_std_2)
+
+    result((x_std_1 == x_expected).all())
+    result((x_std_2 == x_expected_2).all())
+
+standardize_unit_test_method_2(x, x_2, x_expected, x_expected_2)
